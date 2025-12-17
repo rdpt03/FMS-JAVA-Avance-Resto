4a6a1f7b09b6c2777b80a851c3d31c23440f36f8
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-12-17 14:43:29 +0100

    Add askInt function to properly ask for a integer

--- src/Helper.java
+++ src/Helper.java
@@ -47,4 +47,27 @@ public class Helper {
 			
 		}
 	}
+	public static int askInt(Scanner scanner, String question) {
+		while(true) {
+			System.out.print(question+" : ");
+			try {
+				//get the number
+				int chosenEntreeI = scanner.nextInt();
+				System.out.println();	
+				return chosenEntreeI;
+			}
+			//catch errors
+			//not a number
+			catch(InputMismatchException e) {
+				System.out.println("[Erreur] Numero non valide");
+				//spend the scanner token
+				scanner.next();
+			}
+			//unknown error
+			catch(Exception e) {
+				System.out.println("[Erreur] "+e.getMessage());
+			}
+			
+		}
+	}
 }
