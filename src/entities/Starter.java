ec4e91ded204eaf8cb94fdd160558636e2a4995f
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-12-17 12:59:06 +0100

    add child class Starter, parent class is MenuItem

---
+++ src/entities/Starter.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class Starter extends MenuItem{
+
+	public Starter(String name, double price, String description) {
+		super(name, price, description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
