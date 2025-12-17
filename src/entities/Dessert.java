42f166391b502d23f24033e401e14260311718aa
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-12-17 13:01:13 +0100

    add child class Dessert, parent class is MenuItem

---
+++ src/entities/Dessert.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class Dessert extends MenuItem{
+
+	public Dessert(String name, double price, String description) {
+		super(name, price, description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
