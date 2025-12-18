70c07e20fb11364cd9dfb38721789b80a1e8fbd3
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-12-18 13:33:46 +0100

    Create class Menu to store the whole menu

---
+++ src/entities/Menu.java
@@ -0,0 +1,69 @@
+package entities;
+
+public class Menu {
+	private Starter starter;
+	private MainDish mainDish;
+	private SideDish sideDish;
+	private Drink drink;
+	private Dessert dessert;
+	
+	
+	public Menu(Starter starter, MainDish mainDish, SideDish sideDish, Drink drink, Dessert dessert) {
+		super();
+		this.starter = starter;
+		this.mainDish = mainDish;
+		this.sideDish = sideDish;
+		this.drink = drink;
+		this.dessert = dessert;
+	}
+	
+	
+	public Starter getStarter() {
+		return starter;
+	}
+	public void setStarter(Starter starter) {
+		this.starter = starter;
+	}
+	
+	
+	public MainDish getMainDish() {
+		return mainDish;
+	}
+	public void setMainDish(MainDish mainDish) {
+		this.mainDish = mainDish;
+	}
+	
+	
+	public SideDish getSideDish() {
+		return sideDish;
+	}
+	public void setSideDish(SideDish sideDish) {
+		this.sideDish = sideDish;
+	}
+	
+	
+	public Drink getDrink() {
+		return drink;
+	}
+	public void setDrink(Drink drink) {
+		this.drink = drink;
+	}
+	
+	
+	public Dessert getDessert() {
+		return dessert;
+	}
+	public void setDessert(Dessert dessert) {
+		this.dessert = dessert;
+	}
+	
+	@Override
+	public String toString() {
+		return "éntree: "+this.starter.getName()+" | Prix: "+this.starter.getPrice()+
+				"Plat: "+this.mainDish.getName()+" | Prix: "+this.mainDish.getPrice()+
+				"Accompagnement: "+this.sideDish.getName()+" | Prix: "+this.sideDish.getPrice()+
+				"Boisson: "+this.drink.getName()+" | Prix: "+this.drink.getPrice()+
+				"Dessert: "+this.dessert.getName()+" | Prix: "+this.dessert.getPrice();
+	}
+	
+}
